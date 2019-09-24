7e91b534bbf786824f7c1469281947b670a30e24
Paul Charlton <pcharlton@carbonlabs.co.uk>
2019-09-24 13:40:04 +0100

    Add project files.

---
+++ IssueSandbox/IssueSandbox/Views/Issues/CollectionViewLosingItems.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IssueSandbox.Views.Issues
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CollectionViewLosingItems : ContentPage
+    {
+        public CollectionViewLosingItems()
+        {
+            InitializeComponent();
+        }
+    }
+}
